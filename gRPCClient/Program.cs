f80d49d06284e5762b31b4d26de253a8375756ae
senhimadri <senhimadri.dev@gmail.com>
2023-12-27 14:57:20 +0600

    first Commit

---
+++ gRPCClient/Program.cs
@@ -0,0 +1,15 @@
+﻿using Grpc.Net.Client;
+using GrpcServer;
+
+
+var input = new HelloRequest { Name = "Him" };
+
+var channel = GrpcChannel.ForAddress("https://localhost:7257");
+
+var client = new Greeter.GreeterClient(channel);
+
+var replay = await client.SayHelloAsync(input);
+
+Console.WriteLine(replay.Message);
+
+Console.ReadLine();
