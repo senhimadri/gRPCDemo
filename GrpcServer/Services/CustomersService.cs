79653bb7aef52c4855c8f102cf49481f59514338
senhimadri <senhimadri.dev@gmail.com>
2023-12-28 15:02:21 +0600

    Custom Protofile is Created

---
+++ GrpcServer/Services/CustomersService.cs
@@ -0,0 +1,44 @@
+﻿using Grpc.Core;
+
+namespace GrpcServer.Services;
+
+public class CustomersService : Customer.CustomerBase
+{
+
+	private readonly ILogger<CustomersService> _logger;
+
+
+	public CustomersService(ILogger<CustomersService> logger)
+	{
+		_logger = logger;
+	}
+
+    public override Task<CustomerModel> GetCustomerInfo(CustomerLookupModel request, ServerCallContext context)
+    {
+		CustomerModel output = new CustomerModel();
+
+		if(request.UserId==1)
+		{
+			output.FirstName = "Himadri Sen";
+			output.LastName = "Smith";
+        }
+        else if (request.UserId == 2)
+        {
+            output.FirstName = "Alamin Khan";
+            output.LastName = "Sheke";
+        }
+
+        else if (request.UserId == 3)
+        {
+            output.FirstName = "Nakib Choudhori";
+            output.LastName = "Rakib";
+        }
+        else
+        {
+            output.FirstName = "Other";
+            output.LastName = "Other";
+        }
+        return Task.FromResult(output);
+    }
+}
+
